7e712d9484f172e5d56495bb3f1c8d499e67f9d4
TGitVision <60406685+TGitVision@users.noreply.github.com>
2020-07-21 12:15:11 -0400

    Add project files.

---
+++ PasswordGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson10_3PasswordGenerator
+{
+    class PasswordGenerator
+    {
+
+        #region Fields and Properties
+
+        string characterPool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int passwordLength = 8;
+
+        #endregion
+
+        #region Constructors
+        public PasswordGenerator()
+        {
+
+        }
+        public PasswordGenerator(string characterPool, int passwordLength)
+        {
+            if (characterPool.Length > 0 && passwordLength > 0)
+            {
+                this.characterPool = characterPool;
+                this.passwordLength = passwordLength;
+            }            
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string CreatePassword() 
+        {
+
+            Random rnd = new Random();
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < passwordLength; i++) 
+            {
+                int characterIndex = rnd.Next(0, characterPool.Length);
+                password.Append(characterPool.Substring(characterIndex, 1));
+            }
+
+            return password.ToString();
+
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson10_3PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Lesson10_3PasswordGenerator!");
+
+            PasswordGenerator passwordGenerator1 = new PasswordGenerator();
+            Console.WriteLine($"password: {passwordGenerator1.CreatePassword()}");
+
+            PasswordGenerator passwordGenerator2 = new PasswordGenerator(":)TheAmazonClassInCharlotteTakingC#Rocks!", 2);
+            Console.WriteLine($"password 2: {passwordGenerator2.CreatePassword()}");
+
+            PasswordGenerator passwordGenerator4 = new PasswordGenerator(":()TheAmazonClassInCharlotteTakingC#RockAndAreGreatCoders!", 4);            
+            Console.WriteLine($"password 4: {passwordGenerator4.CreatePassword()}");
+
+            PasswordGenerator passwordGenerator6 = new PasswordGenerator(":^)TheAmazonClassInCharlotteTakingC#RockAndWillChangeTheWorld!", 6);
+            Console.WriteLine($"password 6: {passwordGenerator6.CreatePassword()}");
+
+            passwordGenerator4 = new PasswordGenerator("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 4);
+
+            string FirstSet = passwordGenerator4.CreatePassword();
+
+            string SecondSet = passwordGenerator4.CreatePassword();
+
+            string ThirdSet = passwordGenerator4.CreatePassword();
+
+            string FourthSet = passwordGenerator4.CreatePassword();
+
+            Console.WriteLine($"4 Part Hash: {FirstSet} - {SecondSet} - {ThirdSet} - {FourthSet}");
+
+        }
+    }
+}
